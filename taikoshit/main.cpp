d0a8c066688b1abac1b39eba6c7bfd27923d77c5
Mark Wilkens <108659743+schummler@users.noreply.github.com>
2024-07-02 12:30:00 +0200

    simplified bot to get issues

--- taikoshit/main.cpp
+++ taikoshit/main.cpp
@@ -1,20 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <Windows.h>
 #include <chrono>
 #include <thread>
 #include <atomic>
 #include <mutex>
 #include <random>
-void PressKey(UINT key)
-{
-    
 
-    keybd_event(static_cast<BYTE>(key), 0, 0, 0);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    keybd_event(static_cast<BYTE>(key), 0, KEYEVENTF_KEYUP, 0);
-
-    
-}
 int main() {
     
     HWND hwndDesktop = GetDesktopWindow();
@@ -28,11 +19,7 @@ int main() {
     bool botenabled = false;
     bool inslider = false;
     int delay;
-    int offsetmin = 0;
-    int offsetmax = 20;
-    int offset;
 
-    std::cout << "Ahorn Taiko - v0.3 B10.03.2023\n";
     std::cout << "DELETE>  Panic Exit\n";
     std::cout << "F9>  Start Bot\n";
     std::cout << "F10>  Stop Bot\n";
@@ -44,12 +31,11 @@ int main() {
 
         COLORREF currColor = GetPixel(hdcDesktop, 340, 445);
         COLORREF spinner = GetPixel(hdcDesktop, 720, 595);
-        offset = rand() % offsetmax + offsetmin;
         lastNoteTime = std::chrono::high_resolution_clock::now();
         auto now = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastNoteTime);
         lastNoteTime = now;
-        delay = duration.count() + offset;
+        delay = 0;
 
         int red = GetRValue(currColor);
         int green = GetGValue(currColor);
@@ -75,18 +61,7 @@ int main() {
             system("CLS");
             
         }
-        if (GetAsyncKeyState(VK_F12)) {
-            std::cout << "Enter min offset > ";
-            std::cin >> offsetmin;
-            std::cout << "Enter max offset > ";
-            std::cin >> offsetmax;
-        }
-        
-        if (GetAsyncKeyState(VK_F11)) {
-            std::cout << "UR Range " << offsetmin << "ms - " << offsetmax << "ms.\n";
-            Sleep(1000);
 
-        }
         SHORT donkey;
         SHORT kakey;
         SHORT spinnerkey;
@@ -98,20 +73,20 @@ int main() {
 
                 if (lastKeyPressDon)
                 {
-                    
+                    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
                     donkey = VkKeyScan('s');
                     lastKeyPressDon = false;
 
                 }
                 else
                 {
-                    
+                    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
                     donkey = VkKeyScan('k');
                     lastKeyPressDon = true;
 
                 }
 
-                std::cout << "Detection> Don using " << delay << " ms offset. \n";
+                //std::cout << "Detection> Don using " << delay << " ms offset. \n";
                 std::this_thread::sleep_for(std::chrono::milliseconds(delay));
                 keybd_event(donkey, 0, 0, 0);
                 std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -137,79 +112,12 @@ int main() {
 
                 }
 
-                std::cout << "Detection> Ka using " << delay << " ms offset. \n";
+               // std::cout << "Detection> Ka using " << delay << " ms offset. \n";
                 keybd_event(kakey, 0, 0, 0);
                 std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 keybd_event(static_cast<BYTE>(kakey), 0, KEYEVENTF_KEYUP, 0);
                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }
-            
-            if (red >= 245 && red <= 255 && green >= 245 && green <= 255 && blue >= 245 && blue <= 255) {
-                if (lastKeyPressKa)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-                    kakey = VkKeyScan('l');
-                    lastKeyPressKa = false;
-
-                }
-                else
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-                    kakey = VkKeyScan('a');
-                    lastKeyPressKa = true;
-
-                }
-                std::cout << "Dectection> Slidertick Detected!\n";
-                keybd_event(kakey, 0, 0, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                keybd_event(static_cast<BYTE>(kakey), 0, KEYEVENTF_KEYUP, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(5));
-            }
-            
-            if (red >= 245 && red <= 255 && green >= 175 && green <= 190 && blue >= 0 && blue <= 15) {
-                if (lastKeyPressKa)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-                    kakey = VkKeyScan('l');
-                    lastKeyPressKa = false;
-
-                }
-                else
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-                    kakey = VkKeyScan('a');
-                    lastKeyPressKa = true;
-
-                }
-                std::cout << "Dectection> Slider Detected!\n";
-                keybd_event(kakey, 0, 0, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                keybd_event(static_cast<BYTE>(kakey), 0, KEYEVENTF_KEYUP, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }
-            if (sred >= 195 && sred <= 210 && sgreen >= 0 && sgreen <= 10 && sblue >= 170 && sblue <= 185) {
-                
-
-                if (lastKeyPressSpinner)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                    spinnerkey = VkKeyScan('l');
-                    lastKeyPressSpinner = false;
-
-                }
-                else
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                    spinnerkey = VkKeyScan('k');
-                    lastKeyPressSpinner = true;
-
-                }
-                std::cout << "Dectection> Spinner Detected!\n";
-                keybd_event(spinnerkey, 0, 0, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(5));
-                keybd_event(spinnerkey, 0, KEYEVENTF_KEYUP, 0);
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }  
+            }    
         }
     }
             
